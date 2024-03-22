a5f22f00c189abc91a0aa07809b102c137fa5a8d
vakabinto <vakabinto@gmail.com>
2024-03-22 20:25:24 +0100

    iac-files

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3"{
+        bucket = "akadevops"
+        key    = "terraform.tfstate"
+        region = "us-east-1"
+        encrypt = "true"
+        dynamodb_table = "test"
+    }
+}
