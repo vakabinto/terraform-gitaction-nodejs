e730adcdccb30e9123cc02e033268baf02dcc90b
vakabinto <vakabinto@gmail.com>
2024-03-30 05:35:58 +0100

    hf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@
 
 module "vpc" {
   source                       = "terraform-aws-modules/vpc/aws"
-  version                      = "~> 3.0"
+  version                      = "~> 5.0"
   name                         = var.vpc_name
   cidr                         = var.vpc_cidr
   azs                          = var.vpc_azs
